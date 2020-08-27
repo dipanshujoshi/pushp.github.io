570689e87206795dfc7710bd28908aa971bf6153
dipanshujoshi <joshidipanshu2003@gmail.com>
2020-08-27 00:54:22 -0700

    hmmmm

---
+++ contect-form.php
@@ -0,0 +1,25 @@
+<?php
+    $name= $_post['name'];
+	$visitor_email = $_post['email'];
+	$message = $_post['message'];
+	
+	
+	$email_from = 'joshidipanshu2005@gmail.com';
+	
+	$email_subject = 'new form submission';
+	
+	$email_body = "User Name: $name.\n".
+	"User Email: $visitor_email.\n".
+	"User Phone: $phone number.\n".
+	"User Message: $message.\n";
+	
+	$to = "joshidipanshu203@gmail.com";
+	
+	$headers = "From: $email_from \r\n";
+	
+	$headers .= "Reply-To: $visitor_email \r\n";
+	
+	mail($to,$email_subject,$email_body,$headers);
+	
+	header("Location: index.html");
+?>
